743fb249c6cf35d7a25c4bcb42b077e4b65d48ce
Bohdan Korniyenko <bohdan.korniyenko@gmail.com>
2015-07-22 15:55:06 +0300

    add box argument to URL

--- src/discovery_app.erl
+++ src/discovery_app.erl
@@ -20,20 +20,12 @@ start() ->
 start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
       {'_', [
-        %{"/", cowboy_static, {priv_file, discovery, "project1.html"}}
-        {"/", cowboy_static, {file, "priv/project1.html"}},
-%        {"/static/[...]", cowboy_static, {priv_dir, discovery, "static"}},
-        {"/websocket", ws_handler, []}
+        {"/:box", cowboy_static, {file, "priv/project1.html"}},
+        {"/websocket/:box", ws_handler, []}
       ]}
     ]),
     {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
       [{env, [{dispatch, Dispatch}]}]),
-
-  {ok, Pid} = gen_event:start_link({local, drc_event}),
-  gen_event:add_handler(Pid, drc_event, []),
-
-%%   gen_event:notify(drc_event, test),
-%%   timer:apply_interval(2000, gen_event, notify, [drc_event, {notify, <<"hey">>}]),
 
     discovery_sup:start_link().
 
--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -6,12 +6,15 @@
 -record(state, {}).
 
 init(Req, _Opts) ->
+    Box = cowboy_req:binding(box,Req),
+    io:format("ws_handler init, box: ~p~n", [Box]),
     erlang:send_after(2000, self(), add),
     {cowboy_websocket, Req, #state{}}.
 
 websocket_info(add, Req, State) ->
+    %% Send a text to a websocket
     {reply, {text, <<"[]">>}, Req, State}.
 
 websocket_handle(Frame, Req, State) ->
-io:format("ws_handler websocket_handle ~p~n", [Frame]),
+    io:format("ws_handler websocket_handle ~p~n", [Frame]),
     {ok, Req, State}.
