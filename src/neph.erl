63aee7380a0b886db605aa1bd36edee425fc3030
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2015-07-23 12:33:13 +0300

    a new fun added to neph which returns a graph in a form of {Nodes,Edges}; del_wire fixed in web_monitor

--- src/neph.erl
+++ src/neph.erl
@@ -10,7 +10,8 @@
 -module(neph).
 -export([new/1,size/1,
 		has_box/2,
-		boxe_list/1,
+		box_list/1,
+		box_wire_list/1,
 		neighbor/3,
 		neighbor_boxes/2,
 		neighbors/2,
@@ -20,19 +21,51 @@
 		]).
 
 
+
 % creates new network
 new(Box) -> dict:store(Box,[],dict:new()).  
 
+
+
 % returns size of known network
 size(Net_data) -> dict:size(Net_data).
+
 
 
 % returns true/false if Graph has/has_not Node  
 has_box(Box,Net_data) -> dict:is_key(Box,Net_data).
 	
 
+
 % returns the all boxes in the network (known for a given box)
-boxe_list(Net_data) -> dict:fetch_keys(Net_data).
+box_list(Net_data) -> dict:fetch_keys(Net_data).
+
+
+
+% returns a tuple containing box list and wire list. wires represented
+% as tuples containing two indices: (box1_id, box2_id), where index is 
+% zero-base index in the box list
+box_wire_list(Net_data) -> 
+	Box_list = box_list(Net_data),
+	box_wire_list(Net_data, Box_list,lists:zip(Box_list,lists:seq(0,length(Box_list)-1)), [], []).
+
+box_wire_list(Net_data,[Box|To_proc],Boxes,Done_boxes,Acc) ->
+	case lists:member(Box,Done_boxes) of
+		true -> box_wire_list(Net_data,To_proc,Boxes,Done_boxes,Acc);
+		false->
+			Neibs = lists:filter(fun(B)-> not lists:member(B,Done_boxes)
+								end,neighbor_boxes(Box,Net_data)),
+
+			K = lists:keyfind(Box,1,Boxes),
+			{Done_boxes1,Acc1} = lists:foldl(fun(B,{D,A})-> 
+									{B,J} = lists:kefind(B,1,Boxes),
+									{[B|D],[{J,K}|A]}
+											end,{Done_boxes,Acc},Neibs),
+			box_wire_list(Net_data,To_proc,Boxes,[Box|Done_boxes1],Acc1)
+	end;
+box_wire_list(_Net_data,[],Boxes,_Done_boxes,Acc) ->
+	{[ B || {B,_} <- Boxes ],Acc}.
+
 
 
 % returns {Neighbor_port, Neighbor_box} or not_connected
@@ -44,13 +77,18 @@ neighbor(Box_id,Port,Net_data) ->
 	end.
 
 
+
 % returns a list of {port1,port2,box2} triples for each Neighbor
 neighbors(Box_id,Net_data) -> dict:fetch(Box_id,Net_data).
+
+
 
 
 % returns all neighbor boxes for a given box
 neighbor_boxes(Box_id,Net_data) ->
 	[ B || {_,_,B} <- dict:fetch(Box_id,Net_data)].
+
+
 
 
 % returns Net_data1 after removing a connection between boxes
@@ -62,6 +100,7 @@ del_wire(Box1,Port1,Port2,Box2,Net_data) ->
 	Neighbors22=lists:keydelete(Port2,1,Neighbors2),
 
 	dict:store(Box2,Neighbors22,dict:store(Box1,Neighbors11,Net_data)).
+
 
 
 
